575be73c462449f9f8e4fdb22c57f6461b5d9bcb
Satvik Kushwaha <satvik@Satviks-Mac-mini.local>
2025-12-14 17:38:59 +0530

    Revert "old approach for deployment"
    This reverts commit 90dd0db7d90da4be63f6bf63e99a4965f34f32b1.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => ({
   // Add base path for assets when building
   base: mode === "production" ? "/" : "/",
   build: {
-    outDir: "../backend/static",
+    outDir: "dist/",
     emptyOutDir: true,
     assetsDir: "assets", // This ensures assets go into /assets/ folder
   },
